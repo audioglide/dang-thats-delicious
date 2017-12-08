680098549158a3e53839b22267574bdf15b6444d
Alin Tudosie <alin.tudosie@itsix.com>
2017-12-08 15:48:11 +0200

    Added edit store route and form

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -20,3 +20,26 @@ exports.createStore = async (req, res) => {
 	req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
 	res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+	// Query the database for a list of stores
+	const stores = await Store.find();
+	console.log(stores);
+	res.render('stores', {title: 'Stores', stores: stores});
+}
+
+exports.editStore = async (req, res) => {
+	//res.json(req.params);
+	const store = await Store.findOne({_id: req.params.id});
+	res.render('editStore', {title: `Edit ${store.name}`,store: store});
+}
+
+exports.updateStore = async (req, res) => {
+	const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body,{
+		new : true,
+		runValidators: true
+
+	}).exec();
+	req.flash('success', `Successfully updated <strong>${store.name}</strong><a href="/stores/${store.slug}"> View Store &rarr;</a>`);
+	res.redirect(`/stores/${store._id}/edit`);
+};
