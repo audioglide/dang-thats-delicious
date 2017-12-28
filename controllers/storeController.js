c756cd5efa0c54a06c97de7f3796827f61da7823
Alin Tudosie <alin.tudosie@itsix.com>
2017-12-28 15:14:20 +0200

    Added sample data, author and indexes schema

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -44,6 +44,7 @@ exports.resize = async (req, res, next) => {
 }
 
 exports.createStore = async (req, res) => {
+	req.body.author = req.user._id;
 	const store = await (new Store(req.body)).save();
 	req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
 	res.redirect(`/store/${store.slug}`);
@@ -55,9 +56,19 @@ exports.getStores = async (req, res) => {
 	res.render('stores', {title: 'Stores', stores: stores});
 }
 
+const confirmOwner = (store, user) => {
+	if(!store.author.equals(user._id)){
+		throw Error ('You must own a store in order to edit it!');
+	}
+};
+
 exports.editStore = async (req, res) => {
 	//res.json(req.params);
+	// 1. Find the store given the ID
 	const store = await Store.findOne({_id: req.params.id});
+	// 2. Confirm they are the owner of the store
+	confirmOwner(store, req.user);
+	// 3. Render the edit form so the user can update their store
 	res.render('editStore', {title: `Edit ${store.name}`,store: store});
 }
 
@@ -73,7 +84,7 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoreBySlug = async (req, res, next) =>{
-	const store = await Store.findOne({slug: req.params.slug});
+	const store = await Store.findOne({slug: req.params.slug}).populate('author');
 	if(!store) return next();
 	res.render('store', {store, title:store.name});
 }
@@ -87,3 +98,22 @@ exports.getStoresByTag  = async (req, res) => {
 	res.render('tag',{tags, title: 'Tags', tag, stores});
 	//res.json(stores);
 };
+
+exports.searchStores = async (req, res) => {
+	const stores = await Store
+	//first find stores that match
+	.find({
+		$text: {
+			$search: req.query.q
+		}
+	},{
+		score: {$meta: 'textScore'}
+	})
+	//then sort them
+	.sort({
+		score: {$meta:'textScore'}
+	})
+	//limit to only 5 results
+	.limit(5);
+	res.json(stores);
+}
