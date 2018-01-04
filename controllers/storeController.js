d4ab21228dacb980882adc3118d472e2e4d96975
Alin Tudosie <alin.tudosie@itsix.com>
2018-01-04 17:00:33 +0200

    Adde review controller and views

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -85,7 +85,7 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoreBySlug = async (req, res, next) =>{
-	const store = await Store.findOne({slug: req.params.slug}).populate('author');
+	const store = await Store.findOne({slug: req.params.slug}).populate('author reviews');
 	if(!store) return next();
 	res.render('store', {store, title:store.name});
 }
@@ -151,4 +151,11 @@ exports.heartStore = async (req, res) => {
 		{new: true}
 	);
 	res.json(user);
+}
+
+exports.getHearts = async(req, res) =>{
+	const stores = await Store.find({
+		_id: {$in: req.user.hearts}
+	});
+	res.render('stores', {title: 'Hearted stores', stores: stores});
 }
--- models/Store.js
+++ models/Store.js
@@ -9,11 +9,11 @@ const storeSchema = new mongoose.Schema({
 		required: 'Please enter a store name!'
 	},
 	slug: String,
-	description:{
+	description: {
 		type: String,
 		trim: true
 	},
-	tags:[String],
+	tags: [String],
 	created: {
 		type: Date,
 		default: Date.now
@@ -38,7 +38,12 @@ const storeSchema = new mongoose.Schema({
 		ref: 'User',
 		required: 'You must supply an author'
 	}
-});
+},
+	{
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true }
+
+	});
 //Define our indexes
 storeSchema.index({
 	name: 'text',
@@ -49,25 +54,33 @@ storeSchema.index({
 	location: '2dsphere'
 });
 
-storeSchema.pre('save', async function(next){
-	if(!this.isModified('name')){
+storeSchema.pre('save', async function (next) {
+	if (!this.isModified('name')) {
 		next();
 		return;
 	}
 	this.slug = slug(this.name);
-	const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`,'i')
-	const storesWithSlug = await this.constructor.find({slug: slugRegEx});
-	if(storesWithSlug.length){
-		this.slug = `${this.slug}-${storesWithSlug.length+1}`;
+	const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i')
+	const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+	if (storesWithSlug.length) {
+		this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
 	}
 	next();
 })
 
-storeSchema.statics.getTagsList = function(){
+storeSchema.statics.getTagsList = function () {
 	return this.aggregate([
-		{$unwind: '$tags'},
-		{$group: {_id: '$tags', count: {$sum: 1}}},
-		{$sort: {count: -1}}
+		{ $unwind: '$tags' },
+		{ $group: { _id: '$tags', count: { $sum: 1 } } },
+		{ $sort: { count: -1 } }
 	]);
 }
+
+//find reviews where the stores _id property === reviews store property
+storeSchema.virtual('reviews', {
+	ref: 'Review', //what model to link?
+	localField: '_id', //which filend on the store
+	foreignField: 'store' //which filend on the review
+});
+
 module.exports = mongoose.model('Store', storeSchema);
